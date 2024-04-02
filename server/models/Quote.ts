cef455a3ff2a195462fd34d253cd6de24af59a85
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-04-02 23:47:47 +0500

    added models

---
+++ server/models/Quote.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document, InferSchemaType } from "mongoose";
+
+// Define Mongoose schema for Quote
+const QuoteSchema: Schema = new Schema({
+  content: { type: String, required: true },
+  userPrompt: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  favorites: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+type QuoteModel = InferSchemaType<typeof QuoteSchema>;
+
+// Create and export Quote model
+export default mongoose.model<QuoteModel>("Quote", QuoteSchema);
---
+++ server/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document, InferSchemaType } from "mongoose";
+
+// Define Mongoose schema for User
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  favoriteQuotes: [{ type: Schema.Types.ObjectId, ref: "Quote" }],
+  likedQuotes: [{ type: Schema.Types.ObjectId, ref: "Quote" }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+type UserModel = InferSchemaType<typeof UserSchema>;
+
+// Create and export User model
+export default mongoose.model<UserModel>("User", UserSchema);
