12aceb3f8d6528c824af1c0fbaf1a3c3d53dbef3
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-04-22 02:42:41 +0500

    added about us component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'not-found', component: NotfoundComponent },
+  { path: 'about', component: AboutusComponent },
   { path: '**', redirectTo: 'not-found' },
 ];
