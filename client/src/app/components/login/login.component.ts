b8f6c59861c91247eda028062f78eae7b6e6af33
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-04-22 02:30:11 +0500

    minor fix login

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  login(loginForm: NgForm){
+  login(loginForm: NgForm) {
     console.log(loginForm.value);
   }
-
 }
