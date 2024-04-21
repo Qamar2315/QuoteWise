1ce8a4adbec7788641e6762e89eeb6b68839bb58
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-04-22 02:23:35 +0500

    sign up component ready

--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-
+  signup(signupForm: NgForm) {
+    console.log(signupForm.value);
+  }
 }
