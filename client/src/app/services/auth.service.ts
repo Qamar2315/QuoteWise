f91e278cc4a475dddcbd1a244227bab7e8de1f19
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-04-22 13:59:04 +0500

    added env variables and updated gitignore

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -8,11 +8,23 @@ import { User } from '../types/user.type';
 export class AuthService {
   apiUrl = environment.apiUrl;
   apiKey = environment.apiKey;
-  private user: User | undefined;
+  private user: User | null = this.getUserFromLocalStorage();
   constructor() {}
   getUser() {
     return this.user;
   }
+
+  getUserFromLocalStorage() {
+    const id = localStorage.getItem('id');
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('name');
+    if(id && token && username){
+      return {id, token, username};
+    }else{
+      return null;
+    }
+  }
+
   isAuthenticated() {
     if (this.user) {
       return true;
@@ -20,5 +32,4 @@ export class AuthService {
       return false;
     }
   }
-  
 }
--- client/src/app/types/user.type.ts
+++ client/src/app/types/user.type.ts
@@ -1,6 +1,5 @@
 export interface User{
     id:string;
-    name:string;
-    email:string;
+    username:string;
     token:string;
 }
