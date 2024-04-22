a3cdd9f5aa6e50dd56e39f0b18d499a381e5395d
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-04-22 15:50:38 +0500

    home component added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'not-found', component: NotfoundComponent },
   { path: 'about', component: AboutusComponent },
+  { path: '', component: HomeComponent },
   { path: '**', redirectTo: 'not-found' },
 ];
---
+++ client/src/app/components/home/home.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  quotes = [
+    {
+      _id: '6610463a14318f5cad53d77f',
+      content: 'The only way to do great work is to love what you do.',
+      userPrompt: 'What do you love doing?',
+      author: '6610463a14318f5cad53d77b',
+      likes: [],
+      favorites: [],
+      comments: [],
+      createdAt: '2024-04-05T18:43:06.222Z',
+      updatedAt: '2024-04-05T18:43:06.222Z',
+      __v: 0,
+    },
+    {
+      _id: '6610463a14318f5cad53d780',
+      content: "Believe you can and you're halfway there.",
+      userPrompt: 'What do you believe you can do?',
+      author: '6610463a14318f5cad53d77b',
+      likes: [],
+      favorites: [],
+      comments: [],
+      createdAt: '2024-04-05T18:43:06.223Z',
+      updatedAt: '2024-04-05T18:43:06.223Z',
+      __v: 0,
+    },
+    {
+      _id: '6610463a14318f5cad53d781',
+      content:
+        'The future belongs to those who believe in the beauty of their dreams.',
+      userPrompt: 'What are your dreams?',
+      author: '6610463a14318f5cad53d77b',
+      likes: [],
+      favorites: [],
+      comments: ['661ad1fda511f4d84d1385de'],
+      createdAt: '2024-04-05T18:43:06.223Z',
+      updatedAt: '2024-04-05T18:43:06.223Z',
+      __v: 15,
+    },
+  ];
+}
